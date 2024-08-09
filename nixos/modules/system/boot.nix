1ea20b321550cd886a39519a15bc62173cfbf222
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-08-09 17:41:18 -0400

    feat(boot): better mount parameters for my HDD (2)

--- nixos/modules/system/boot.nix
+++ nixos/modules/system/boot.nix
@@ -32,7 +32,7 @@
   fileSystems."/mnt/Juegos" = {
     device = "/dev/disk/by-label/Juegos";
     fsType = "ext4";
-    options = [ "nofail" "x-systemd.device-timeout=5" "defaults" "noatime" "data=writeback" "nobh" ];
+    options = [ "nofail" "x-systemd.device-timeout=5" "rw" "user" "exec" "relatime" "data=writeback" "nobh" "async" ];
   };
 
   # Configure console (TTY) keymap
