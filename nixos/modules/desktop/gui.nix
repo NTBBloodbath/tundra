b474b4d391dda5fab750027d85212e798e1681c2
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-08-13 18:29:05 -0400

    fix(gui): manually add the lactd systemd service, see the comments in the file

--- nixos/modules/desktop/gui.nix
+++ nixos/modules/desktop/gui.nix
@@ -14,4 +14,16 @@
     lact # AMDGPU Controller
     bottles # Wine gaming my beloved
   ];
+
+  # NOTE: see https://github.com/NixOS/nixpkgs/issues/317544
+  systemd.services.lactd = {
+    description = "AMDGPU Control Daemon";
+    after = ["multi-user.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+      Nice = -10;
+    };
+    enable = true;
+  };
 }
