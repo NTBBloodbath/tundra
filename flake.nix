95394587abdfb622334f827070272830035081e6
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-11-24 16:15:53 -0400

    feat: add playit agent flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,12 @@
 {
   description = "Goofy ahhh system configuration";
 
+  # Binary cache to improve the build time of playit
+  nixConfig = {
+    extra-substituters = [ "https://playit-nixos-module.cachix.org" ];
+    extra-trusted-public-keys = [ "playit-nixos-module.cachix.org-1:22hBXWXBbd/7o1cOnh+p0hpFUVk9lPdRLX3p5YSfRz4=" ];
+  };
+
   inputs = {
     # Nixpkgs unstable
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -15,12 +21,16 @@
       inputs.nixpkgs-unstable.follows = "nixpkgs";
       inputs.nixpkgs-stable.follows = "nixpkgs";
     };
+
+    # Playit.gg agent
+    playit-nixos-module.url = "github:pedorich-n/playit-nixos-module";
   };
 
   outputs = {
     nixpkgs,
     nix-flatpak,
     ghostty,
+    playit-nixos-module,
     ...
   } @ inputs: {
     nixosConfigurations.tundra = nixpkgs.lib.nixosSystem {
@@ -28,6 +38,7 @@
       specialArgs = {inherit inputs;};
       modules = [
         nix-flatpak.nixosModules.nix-flatpak
+        playit-nixos-module.nixosModules.default
         ./nixos/configuration.nix
       ];
     };
