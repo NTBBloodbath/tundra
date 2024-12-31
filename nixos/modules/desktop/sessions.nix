2f0585b088807691cfeefbb08fbf9acf6bc3c157
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-12-31 12:39:17 -0400

    feat(sessions): add swaybg for hyprland

--- nixos/modules/desktop/sessions.nix
+++ nixos/modules/desktop/sessions.nix
@@ -56,6 +56,7 @@
     mpvpaper # Animated wallpapers from MP4
     rofi-wayland # App launcher
     playerctl # Control audio
+    swaybg # Wallpaper
     swaynotificationcenter # Notification daemon and center
     wlogout # Logout menu
     # swayosd # GNOME-like OSD
