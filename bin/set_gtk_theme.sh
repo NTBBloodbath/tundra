b98ec6e794a617ad9924e83a772f57aea9a3de2c
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-08-17 10:58:55 -0400

    feat: better GTK theme script, also toggle Quickshell night light

--- bin/set_gtk_theme.sh
+++ bin/set_gtk_theme.sh
@@ -5,12 +5,16 @@ IFS=$'\n'
 # 24-hours format
 current_hour="$(date "+%H")"
 
-if [ "${current_hour}" -ge 18 ]; then
+# From 6 pm to 5:59 am: dark theme
+# From 6 am to 5:59 pm: light theme
+if [ "${current_hour}" -ge 18 ] || [ "${current_hour}" -lt 6 ]; then
     echo "Setting system color-scheme to dark..."
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
     if [ "$(qs list --all)" != "No running instances." ]; then
         echo "Setting Quickshell theme to dark..."
         qs -c DankMaterialShell ipc call theme dark
+        echo "Setting Quickshell night light to true..."
+        qs -c DankMaterialShell ipc call night enable
     fi
 else
     echo "Setting system color-scheme to light..."
@@ -18,5 +22,7 @@ else
     if [ "$(qs list --all)" != "No running instances." ]; then
         echo "Setting Quickshell theme to light..."
         qs -c DankMaterialShell ipc call theme light
+        echo "Setting Quickshell night light to false..."
+        qs -c DankMaterialShell ipc call night disable
     fi
 fi
