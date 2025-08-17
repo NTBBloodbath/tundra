8a03a88a070a8e08b05299751e988a1eb2759e8e
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-08-17 11:52:44 -0400

    feat: add missing MPD configuration

--- nixos/modules/services/mpd.nix
+++ nixos/modules/services/mpd.nix
@@ -8,7 +8,34 @@
     enable = true;
     startWhenNeeded = true;
 
+    user = "amartin";
+
     musicDirectory = "/mnt/Storage/Music";
     # network.listenAddress = "any";
+
+    extraConfig = ''
+      audio_output {
+        type   "fifo"
+        name   "mpd_fifo"
+        path   "/tmp/mpd.fifo"
+        format "44100:16:2"
+      }
+
+      audio_output {
+        type "pipewire"
+        name "Pipewire Output"
+      }
+    '';
   };
+
+  # See https://wiki.nixos.org/wiki/MPD PipeWire workaround section
+  systemd.services.mpd.environment = {
+    # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
+    XDG_RUNTIME_DIR = "/run/user/1000";
+  };
+
+  # So that tools like Playerctl are able to interact with MPD
+  environment.systemPackages = [
+    pkgs.mpdris2 # MPRIS 2 support for mpd
+  ];
 }
